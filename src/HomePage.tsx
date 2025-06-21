eb7963eebc872bf7242c9ee71b3692daafdb29d7
Aravind <aromalaravind0@gmail.com>
2025-06-21 19:41:41 +0530

    gradient

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -4,6 +4,7 @@ import Manifesto from "./components/ManifestoPage/manifestoPage";
 import { Navbar } from "./components/Navbar/navbar";
 import { LogoSlider } from "./components/LogoSlider/logoSlider";
 import { Muverse } from "./components/muVresePage/muVersePage";
+import logo1 from "./assets/logos/logopm.png";
 const HomePage = () => {
   return (
     <div>
@@ -21,12 +22,14 @@ const HomePage = () => {
         {/* Background Gradient */}
         <div className="p-[250px] right-[-50px] top-[-150px] absolute bg-[#6F00CD] bg-opacity-20 rounded-full blur-[100px]"></div>
 
-        <div className="p-[250px] left-[-90px] top-[150px] absolute bg-purple-600 bg-opacity-30 rounded-full blur-[100px]"></div>
+        <div className="p-[250px] left-[-90px] bottom-[10px] absolute bg-purple-600 bg-opacity-40 rounded-full blur-[100px]"></div>
 
         <div className="relative z-10 h-[100vh]  flex flex-col items-center justify-center  ">
-          <h1 className="text-[#8c3bc3] font-[Montesrrat] leading-none text-[2.5rem] md:text-[6rem] text-center font-bold">
+          <h1 className="bg-clip-text text-transparent bg-gradient-to-tr from-purple-600  to-fuchsia-600 font-[Montesrrat] leading-none text-[2.5rem] md:text-[6rem] text-center font-bold">
             We Are The <br />
-            Purple Movement
+            <span className="flex flex-row">
+              <p>Purple Movement</p>
+            </span>
           </h1>
           <p className="text-white md:text-[1.5rem] text-[1rem] p-3 mt-8 text-center ">
             Rebuilding how India learns-Beyond Borders, Beyond Syllabus, Beyond
@@ -42,9 +45,13 @@ const HomePage = () => {
             <LogoSlider />
           </div>
         </div>
+        <div className="relative">
+          <div className="p-[20px] w-[20%] h-[20%] rounded-full  top-1/2 left-1/2 transform -translate-x-1/2 absolute bg-purple-700 blur-[100px]"></div>
+          <Muverse />
+        </div>
 
-        <div>
-          <Muverse/>
+        <div className="relative">
+          <div className="p-[20px] w-full h-[100vh] absolute bg-gradient-to-br from-purple-700/40 to-transparent blur-[100px] "></div>
           <Manifesto />
         </div>
 
